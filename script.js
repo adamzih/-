1f9aeff7a9ca696848fd88caed4d47c299b1d001
ADAMZIH <120345178+adamzih@users.noreply.github.com>
2025-02-17 10:50:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentInput = "";
+
+function appendNumber(number) {
+    currentInput += number;
+    document.getElementById('display').value = currentInput;
+}
+
+function appendOperator(operator) {
+    currentInput += " " + operator + " ";
+    document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    document.getElementById('display').value = currentInput;
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        document.getElementById('display').value = currentInput;
+    } catch (error) {
+        document.getElementById('display').value = "Ошибка";
+    }
+}
